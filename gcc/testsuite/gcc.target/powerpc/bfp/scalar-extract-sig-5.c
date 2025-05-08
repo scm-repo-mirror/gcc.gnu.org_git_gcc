25c1f7efc152b077842e4e84da93e788e629826a
Alexandre Oliva <oliva@adacore.com>
2025-05-08 02:17:34 -0300

    [testsuite] [ppc] add -mfloat128 to __ieee128-using bfp tests
    Some ppc bfp tests use __ieee128 without ensuring it's available.
    Require ppc_ieee128_ok, add -mfloat128 to get the type defined,
    and tolerate the warning that this option may trigger.
    
    
    for  gcc/testsuite/ChangeLog
    
    	* gcc.target/powerpc/bfp/scalar-extract-exp-5.c: Require
    	ppc_ieee128_ok, add -mfloat128, tolerate its warning.
    	* gcc.target/powerpc/bfp/scalar-extract-sig-5.c: Likewise.
    	* gcc.target/powerpc/bfp/scalar-insert-exp-11.c: Likewise.
    	* gcc.target/powerpc/bfp/scalar-insert-exp-8.c: Likewise.
    	* gcc.target/powerpc/bfp/scalar-test-data-class-11.c: Likewise.
    	* gcc.target/powerpc/bfp/scalar-test-neg-5.c: Likewise.

--- gcc/testsuite/gcc.target/powerpc/bfp/scalar-extract-sig-5.c
+++ gcc/testsuite/gcc.target/powerpc/bfp/scalar-extract-sig-5.c
@@ -1,7 +1,9 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
-/* { dg-options "-mdejagnu-cpu=power9 -mvsx" } */
+/* { dg-options "-mdejagnu-cpu=power9 -mvsx -mfloat128" } */
 /* { dg-require-effective-target ilp32 } */
+/* { dg-require-effective-target ppc_ieee128_ok } */
 /* { dg-require-effective-target powerpc_vsx } */
+/* { dg-prune-output ".-mfloat128. option may not be fully supported" } */
 
 /* This test only runs on 32-bit configurations, producing a compiler
    error because the builtin requires 64 bits.  */
--- gcc/testsuite/gcc.target/powerpc/bfp/scalar-test-neg-5.c
+++ gcc/testsuite/gcc.target/powerpc/bfp/scalar-test-neg-5.c
@@ -1,6 +1,8 @@
 /* { dg-do compile { target { powerpc*-*-* } } } */
-/* { dg-options "-mdejagnu-cpu=power8 -mvsx" } */
+/* { dg-options "-mdejagnu-cpu=power8 -mvsx -mfloat128" } */
+/* { dg-require-effective-target ppc_ieee128_ok } */
 /* { dg-require-effective-target powerpc_vsx } */
+/* { dg-prune-output ".-mfloat128. option may not be fully supported" } */
 
 #include <altivec.h>
 #include <stdbool.h>
